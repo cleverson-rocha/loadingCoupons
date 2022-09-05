430b2ba137da6a64c7f81a5d5412532ede6d0e61
Cleverson Rocha <cleverson.rocha@minu.co>
2022-09-05 17:05:12 -0300

    Primeira chamada no banco e projeção

---
+++ couponBase/README.md
@@ -0,0 +1,7 @@
+1 - Validar a conexão com o banco
+2- get
+3- update upsert
+
+batches
+
+https://nodejs.org/api/stream.html#organization-of-this-document
--- couponBase/index.js
+++ couponBase/index.js
@@ -4,18 +4,80 @@ const { getDb } = require('./db');
 const start = async () => {
   try {
     await connectDb();
-    const arr = await getDb('bonuz', 'experiences').find({ name: 'carrefour-meu-game-carrefour' }).toArray()
-    console.log(arr[0].prizes.length)
+
+    const arrayMany = await getDb('bonuz', 'prizes').find(
+      {
+        "active": true,
+        "deliveryEngine": "coupon"
+      }).project(
+        {
+          "name": 1.0,
+          "_id": 0
+        }
+      ).toArray()
+
+    const prizeArray = arrayMany.map((prize) => prize.name)
+    console.log(prizeArray)
+
+    // for...of
 
   } catch (err) {
     console.error(err);
+  } finally {
+    await disconnectDb()
   };
 }
 
 start();
 
+// db = db.getSiblingDB("bonuz");
+// db.getCollection("prizes").find(
+//   {
+//     "active": true,
+//     "deliveryEngine": "coupon"
+//   },
+//   {
+//     "name": 1.0
+//   }
+// );
 
 
 
-// delivery engine coupon
-// if carrefour
+
+
+
+
+
+
+
+
+
+
+
+// db = db.getSiblingDB("bonuzCoupon");
+// db.getCollection("coupons").find(
+//   {
+//     "bucket": {
+//       "$in": [
+//         "carrefour-eco-tupper-500ml",
+//         "carrefour-eco-tupper-1l",
+//         "carrefour-tupperfresh-quadrado-200ml-2-un",
+//         "carrefour-tupperfresh-quadrado-1l",
+//         "carrefour-tupperfresh-retangular-1-6l",
+//         "carrefour-cj-super-instantaneas-slim-1-3l-2-25l-2-un",
+//         "carrefour-cj-tigelas-maravilhosas-500ml-1l-1-8l-3-un",
+//         "carrefour-eco-tupper-1l-moeda-dinheiro",
+//         "carrefour-tupperfresh-quadrado-200ml-2-un-moeda-dinheiro",
+//         "carrefour-tupperfresh-quadrado-1l-moeda-dinheiro",
+//         "carrefour-tupperfresh-retangular-1-6l-moeda-dinheiro",
+//         "carrefour-cj-super-instantaneas-slim-1-3l-2-25l-2-un-moeda-dinheiro",
+//         "carrefour-cj-tigelas-maravilhosas-500ml-1l-1-8l-3-un-moeda-dinheiro",
+//         "carrefour-turbo-chef-300ml-moeda-dinheiro"
+//       ]
+//     }
+//   }
+// ).sort(
+//   {
+//     "_id": -1.0
+//   }
+// );
