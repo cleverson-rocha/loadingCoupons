d1575ec451bf0b82d2f4eb1ad693740feede0d98
Cleverson Rocha <cleverson.rocha@minu.co>
2022-09-07 18:47:08 -0300

    Teste de inserção de 2mi de cupons no banco local

--- couponBase/index.js
+++ couponBase/index.js
@@ -1,5 +1,4 @@
 const cliProgress = require('cli-progress');
-const chalk = require('chalk');
 const randomString = require('randomstring');
 const { connectDb, disconnectDb } = require('./db');
 const { getDb } = require('./db');
@@ -37,68 +36,74 @@ const start = async () => {
     });
 
     // for...of
-    for (const prizeList of prizeArray) {
+    let i = 1;
+    while (i < 2000) {
 
-      //Gerador de código
-      const alphanumeric = randomString.generate({
-        length: 12,
-        charset: 'alphanumeric'
-      });
-      const stringCode = alphanumeric.toUpperCase()
+      for (const prizeList of prizeArray) {
 
-      await getDb('bonuzCoupon', 'testeCoupons').insertMany([
-        {
-          "alliance": "teste_01",
-          "bucket": prizeList,
-          "coupon": stringCode,
-          "expirationDate": "2022-12-31T12:00:00.000+0000",
-          "initialDateAvaliable": "2022-09-07T12:00:00.000+0000",
-          "created": "2022-09-07T12:00:00.000+0000",
-          "status": {
-            "name": "issued",
-            "timestamp": "2021-06-29T13:37:10.916+0000",
-            "detail": {
-              "expirationDate": "2021-12-31T03:00:00.000Z"
-            }
-          },
-          "trace": [
-            {
+        // Gerador de código
+        const alphanumeric = randomString.generate({
+          length: 12,
+          charset: 'alphanumeric'
+        });
+        const stringCode = alphanumeric.toUpperCase()
+
+        await getDb('bonuzCoupon', 'testeCoupons').insertMany([
+          {
+            "alliance": "teste_01",
+            "bucket": prizeList,
+            "coupon": stringCode,
+            "expirationDate": "2022-12-31T12:00:00.000+0000",
+            "initialDateAvaliable": "2022-09-07T12:00:00.000+0000",
+            "created": "2022-09-07T12:00:00.000+0000",
+            "status": {
               "name": "issued",
               "timestamp": "2021-06-29T13:37:10.916+0000",
               "detail": {
                 "expirationDate": "2021-12-31T03:00:00.000Z"
               }
             },
-            {
-              "name": "created",
-              "detail": {
-                "expirationDate": "2022-12-31T12:00:00.000+0000"
+            "trace": [
+              {
+                "name": "issued",
+                "timestamp": "2021-06-29T13:37:10.916+0000",
+                "detail": {
+                  "expirationDate": "2021-12-31T03:00:00.000Z"
+                }
               },
+              {
+                "name": "created",
+                "detail": {
+                  "expirationDate": "2022-12-31T12:00:00.000+0000"
+                },
+                "timestamp": "2022-09-07T12:00:00.000+0000"
+              }
+            ],
+            "batch": {
+              "name": "teste-teste-01-2022-09-07T12:00:00.500Z",
               "timestamp": "2022-09-07T12:00:00.000+0000"
             }
-          ],
-          "batch": {
-            "name": "teste-teste-01-2022-09-07T12:00:00.500Z",
-            "timestamp": "2022-09-07T12:00:00.000+0000"
           }
-        }
-      ]);
-      // console.log(prizeList);
+        ]);
+      };
+      // Incremento da variável de controle While
+      i++;
 
+      // // Calculo de iteração
+      // const totalProgress = i * prizeArray.length
+
+      // // create new progress bar
+      // const barProgress = new cliProgress.SingleBar({}, cliProgress.Presets.legacy);
+      // barProgress.start(totalProgress, 0);
+      // barProgress.update(stringCode);
+      // barProgress.stop();
     };
-
-    // Progress bar
-    const barProgress = new cliProgress.SingleBar({}, cliProgress.Presets.legacy);
-
-    barProgress.start(prizeArray.length, 0);
-    barProgress.update(prizeArray.length);
-    barProgress.stop();
 
   } catch (err) {
     console.error(err);
   } finally {
     await disconnectDb()
   };
-}
+};
 
 start();
